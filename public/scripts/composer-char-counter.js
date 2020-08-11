01cd4fef27e15f64a2be92f49cdeb2913d247389
Sam Yang <samyang8989@gmail.com>
2020-08-11 03:44:08 +0000

    Implement character count for #tweet-text form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,12 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$('#tweet-text').keyup(function () {
+		const text = $(this).val().length;
+		const counter = $(this).siblings('.tweet-text_submit').children('.counter');
+		const currentCount = parseInt(140 - text);
+
+		currentCount < 0 ? counter.css('color', 'red') : counter.css('color', '#444444');
+		counter.html(currentCount);
+	});
+});
+
+// Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque explicabo in ab praesentium minus distinctio, odit, ducimus dolores temp
