3d9732e6c16c17cef0ca83e51d0fb6af29124418
Sam Yang <samyang8989@gmail.com>
2020-08-11 16:09:27 +0000

    Update variables with $ to indicate jQuery variables

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 	$('#tweet-text').keyup(function () {
-		const text = $(this).val().length;
-		const counter = $(this).siblings('.tweet-text_submit').children('.counter');
-		const currentCount = parseInt(140 - text);
+		const $text = $(this).val().length;
+		const $counter = $(this).siblings('.tweet-text_submit').children('.counter');
+		const $currentCount = parseInt(140 - $text);
 
-		currentCount < 0 ? counter.css('color', 'red') : counter.css('color', '#444444');
-		counter.html(currentCount);
+		$currentCount < 0 ? $counter.css('color', 'red') : $counter.css('color', '#444444');
+		$counter.html($currentCount);
 	});
 });
 
