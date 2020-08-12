897b3a5b619b8c84a8ac7666fa895d0e33de869b
Sam Yang <samyang8989@gmail.com>
2020-08-12 03:02:29 +0000

    Fetch tweets with Ajax GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,6 @@
 // Client-side JS logic goes here // jQuery is already loaded // Reminder: Use (and do all your DOM work in) jQuery's document ready function
 
 $(document).ready(function () {
-	const data = [
-		{
-			user: {
-				name: 'Newton',
-				avatars: 'https://i.imgur.com/73hZDYK.png',
-				handle: '@SirIsaac',
-			},
-			content: {
-				text: 'If I have seen further it is by standing on the shoulders of giants',
-			},
-			created_at: 1461116232227,
-		},
-		{
-			user: {
-				name: 'Descartes',
-				avatars: 'https://i.imgur.com/nlhLi3I.png',
-				handle: '@rd',
-			},
-			content: {
-				text: 'Je pense , donc je suis',
-			},
-			created_at: 1461113959088,
-		},
-	];
-
 	// const daysAgo = (createdAt) => {
 	// 	const curTime = new Date().getTime();
 	// 	return Math.floor((curTime - createdAt) / (60 * 60 * 24 * 1000));
@@ -59,8 +34,6 @@ $(document).ready(function () {
 		return $tweet;
 	};
 
-	renderTweets(data);
-
 	$('#new-tweet_form').submit(function (event) {
 		event.preventDefault();
 		const post_url = $(this).attr('action');
@@ -85,4 +58,13 @@ $(document).ready(function () {
 		// 		console.error(err);
 		// 	});
 	});
+
+	const loadTweets = function () {
+		$.get('/tweets', function (data, status) {
+			console.log('data', data, 'status', status);
+			renderTweets(data);
+		});
+	};
+
+	loadTweets();
 });
