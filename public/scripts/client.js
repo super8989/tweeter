9d8a207acc667ee9449622a8d70a78e40971f74a
Sam Yang <samyang8989@gmail.com>
2020-08-12 00:39:56 +0000

    Implement renderTweets to loop through tweet objects to display them

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,46 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Client-side JS logic goes here // jQuery is already loaded // Reminder: Use (and do all your DOM work in) jQuery's document ready function
 
 $(document).ready(function () {
-	const tweetData = {
-		user: {
-			name: 'Newton',
-			avatars: 'https://i.imgur.com/73hZDYK.png',
-			handle: '@SirIsaac',
+	const data = [
+		{
+			user: {
+				name: 'Newton',
+				avatars: 'https://i.imgur.com/73hZDYK.png',
+				handle: '@SirIsaac',
+			},
+			content: {
+				text: 'If I have seen further it is by standing on the shoulders of giants',
+			},
+			created_at: 1461116232227,
 		},
-		content: {
-			text: 'If I have seen further it is by standing on the shoulders of giants',
+		{
+			user: {
+				name: 'Descartes',
+				avatars: 'https://i.imgur.com/nlhLi3I.png',
+				handle: '@rd',
+			},
+			content: {
+				text: 'Je pense , donc je suis',
+			},
+			created_at: 1461113959088,
 		},
-		created_at: 1461116232227,
-	};
+	];
 
 	// const daysAgo = (createdAt) => {
 	// 	const curTime = new Date().getTime();
 	// 	return Math.floor((curTime - createdAt) / (60 * 60 * 24 * 1000));
 	// };
 
+	// loop through tweets and append the return value to tweets container
+	const renderTweets = function (tweets) {
+		for (let tweet of tweets) {
+			$('#tweets-container').append(createTweetElement(tweet));
+		}
+		return;
+	};
+
 	const createTweetElement = (tweetObj) => {
-		return `<article class='tweet_box'>
+		let $tweet = `<article class='tweet_box'>
         <header>
           <img class='tweet_avatar' src='${tweetObj.user.avatars}' alt='avatar'><span>${tweetObj.user.name}</span>
           <span class='tweet_handle'>${tweetObj.user.handle}</span>
@@ -38,9 +55,9 @@ $(document).ready(function () {
           </div>
         </footer>
       </article>`;
+
+		return $tweet;
 	};
 
-	const $tweet = createTweetElement(tweetData);
-
-	$('#tweets-container').append($tweet);
+	renderTweets(data);
 });
